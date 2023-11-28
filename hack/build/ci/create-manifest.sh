99d295d1d1746a27e480bc120fbadaa7770d183d
Gabriel Krenn <gabriel.krenn@gmail.com>
2023-11-28 14:43:22 +0100

    Remove unused registries

--- hack/build/ci/create-manifest.sh
+++ hack/build/ci/create-manifest.sh
@@ -10,17 +10,8 @@ image_name=$1
 image_tag=$2
 multiplatform=$3
 
-if [ "$multiplatform" == "true" ]
-then
-  echo "Creating manifest for AMD, ARM and PPC64LE images"
-  docker pull "${image_name}:${image_tag}-amd64"
-  docker pull "${image_name}:${image_tag}-arm64"
-  docker pull "${image_name}:${image_tag}-ppc64le"
-  docker manifest create "${image_name}:${image_tag}" "${image_name}:${image_tag}-arm64" "${image_name}:${image_tag}-amd64" "${image_name}:${image_tag}-ppc64le"
-else
-  echo "Creating manifest for the AMD image "
-  docker pull "${image_name}:${image_tag}-amd64"
-  docker manifest create "${image_name}:${image_tag}" "${image_name}:${image_tag}-amd64"
-fi
+echo "Creating manifest for the AMD image "
+docker pull "${image_name}:${image_tag}-amd64"
+docker manifest create "${image_name}:${image_tag}" "${image_name}:${image_tag}-amd64"
 
 docker manifest push "${image_name}:${image_tag}"
